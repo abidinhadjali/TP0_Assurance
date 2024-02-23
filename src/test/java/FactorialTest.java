302127fdab644f11482876e50982d73928d80392
HadjAli-Ahmed <Abidinhadjali4@gmail.com>
2024-02-23 23:44:02 +0100

    Exo4 complited

---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    void factorialSholdRiseExeptionIfTheElementISNegative() {
+        Assertions.assertThrows(  IllegalArgumentException.class,()->Factorial.factorial(-2));
+    }
+    @Test
+    void factorialShouldReturnZeroIfTheElemntIsZero(){
+
+        Assertions.assertEquals(0, Factorial.factorial(100));
+
+    }
+}
